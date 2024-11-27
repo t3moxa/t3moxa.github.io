1d2a8b8480a95b30e085c7b6da6c51e71a90bbad
t3moxa <133843440+t3moxa@users.noreply.github.com>
2024-11-27 16:07:18 +0700

    V14

--- app.js
+++ app.js
@@ -8,3 +8,28 @@ if ('serviceWorker' in navigator) {
         });
     });
 }
+
+function onLoad() {
+	for(let i=0; i<localStorage.length; i++) {
+		let key = localStorage.key(i);
+		addToPage(key)
+	}
+}
+
+function buttonClick() {
+	let login = document.getElementById("login").value; 
+	let password = document.getElementById("password").value;
+	if ((login == "") || (password == "")) {
+		alert("Логин и/или пароль не введён!")
+	} else {
+		localStorage[login] = password;
+		addToPage(login);
+	}
+}
+
+function addToPage(key) {
+	let loginList = document.getElementById("login_list")
+	let p = document.createElement("p");
+	p.innerText = key + " " + localStorage[key];
+	loginList.append(p);
+}
